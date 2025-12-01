577c9ba918b6d2f6d868f33a56191397b0ef9203
Franco <franco_tigres18@hotmail.com>
2025-12-01 10:07:42 -0600

    feat:add type gallery and component grid

--- src/types/index.ts
+++ src/types/index.ts
@@ -29,6 +29,15 @@ export const BaseWPSchema = z.object({
   }),
 });
 
+const gallerySchema = z.object({
+  large: imageSchema,
+  full: imageSchema,
+});
+
+export const GalleryPageSchema = BaseWPSchema.extend({
+  gallery: z.array(gallerySchema),
+});
+
 const processSchema = z.object({
   title: z.string(),
   description: z.string(),
@@ -60,3 +69,4 @@ export const PostSchema = BaseWPSchema.omit({
 export const PostsSchema = z.array(PostSchema);
 
 export type Post = z.infer<typeof PostSchema>;
+export type Gallery = z.infer<typeof gallerySchema>;
